2e405c16356290c29af361ef71703f138c8c21a2
Huang Hai <hh81300889@gmail.com>
2026-06-11 16:25:15 +0800

    fix: 修正默认温度迁移版本号长度

--- alembic/versions/20260611_allow_prompt_test_default_temperature.py
+++ alembic/versions/20260611_allow_prompt_test_default_temperature.py
@@ -1,6 +1,6 @@
 """allow prompt test units to use provider default temperature
 
-Revision ID: 20260611_allow_default_temperature
+Revision ID: 20260611_temp_default
 Revises: 20260609_ai_scoring
 Create Date: 2026-06-11 15:35:00.000000
 
@@ -12,7 +12,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision: str = "20260611_allow_default_temperature"
+revision: str = "20260611_temp_default"
 down_revision: Union[str, None] = "20260609_ai_scoring"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
